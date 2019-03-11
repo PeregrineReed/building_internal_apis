388ca43a41bc6c56328a1022c09f6c36e9ddf472
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-03-11 15:20:13 -0600

    Completed exercise

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -7,5 +7,52 @@ describe "Items API" do
     get '/api/v1/items'
 
     expect(response).to be_successful
+
+    items = JSON.parse(response.body)
+
+    expect(items.count).to eq(3)
+  end
+
+  it 'can get one tiem by its id' do
+    id = create(:item).id
+
+    get "/api/v1/items/#{id}"
+
+    item = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(item["id"]).to eq(id)
+  end
+
+  it 'can create a new item' do
+    item_params = { name: "Saw", description: "I want to play a game" }
+
+    post "/api/v1/items", params: {item: item_params}
+    item = Item.last
+
+    expect(response).to be_successful
+    expect(item.name).to eq(item_params[:name])
+  end
+
+  it 'can update an existing item' do
+    id = create(:item).id
+    previous_name = Item.last.name
+    item_params = { name: "Sledge" }
+
+    put "/api/v1/items/#{id}", params: {item: item_params}
+    item = Item.find(id)
+
+    expect(response).to be_successful
+    expect(item.name).to_not eq(previous_name)
+    expect(item.name).to eq("Sledge")
+  end
+
+  it 'can destroy an item' do
+    item = create(:item)
+    
+    expect{ delete "/api/v1/items/#{item.id}" }.to change(Item, :count).by(-1)
+
+    expect(response).to be_successful
+    expect{Item.find(item.id)}.to raise_error(ActiveRecord::RecordNotFound)
   end
 end
